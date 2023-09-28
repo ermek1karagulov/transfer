b52e4149194c6ab47cd89b0487f2d5958b54d6e4
Ermek Karagulov <ermekkaragulov0@gmail.com>
2023-09-28 17:19:51 +0600

    ermek

--- src/component/information/Information.tsx
+++ src/component/information/Information.tsx
@@ -10,7 +10,7 @@ const animationScroll = {
   visible: (custom: number) => ({
     y: 0,
     opacity: 1,
-    transition: { delay: custom * 0.3 },
+    transition: { delay: custom * 0.2 },
   }),
 };
 
