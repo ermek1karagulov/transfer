b6a2d312c65b22caf2cefb1c8552f19fc2e91e39
Ermek Karagulov <ermekkaragulov0@gmail.com>
2023-10-04 18:40:54 +0600

    the end 8

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -111,7 +111,7 @@ export default function Navbar(props: Props) {
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
+            keepMounted: true,
           }}
           sx={{
             display: { xs: "block", sm: "none" },
