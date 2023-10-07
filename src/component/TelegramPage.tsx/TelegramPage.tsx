4d9458df94f321e5a066fb903408d9760c477218
Ermek Karagulov <ermekkaragulov0@gmail.com>
2023-10-07 17:35:53 +0600

    ufffff9

--- src/component/TelegramPage.tsx/TelegramPage.tsx
+++ src/component/TelegramPage.tsx/TelegramPage.tsx
@@ -147,6 +147,7 @@ const TelegramPage = () => {
               display: "flex",
               //   padding: "0rem 0rem 0rem 0rem",
               justifyContent: "center",
+              width: "100%",
             }}
           >
             <Box
@@ -157,31 +158,29 @@ const TelegramPage = () => {
               noValidate
               autoComplete="off"
             >
-              <div>
-                <TextField
-                  id="outlined-basic"
-                  label="Вы отправляете"
-                  variant="outlined"
-                  // style={{ width: "100%" }}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  value={inputData.sell}
-                  onChange={(e) => {
-                    //@ts-ignore
-                    if (!/\d+/.test(Number(e.target.value))) return;
-                    const som = Number(e.target.value) * rate[CurrencyType.RUB];
-                    const converted = som
-                      ? Number(som) / rate[selectedBuyCurrency]
-                      : 0;
-                    setInputData((v) => ({
-                      ...v,
-                      sell: Number(e.target.value),
-                      buy: converted - (converted / 100) * 5,
-                    }));
-                  }}
-                />
-              </div>
+              <TextField
+                id="outlined-basic"
+                label="Вы отправляете"
+                variant="outlined"
+                // style={{ width: "100%" }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={inputData.sell}
+                onChange={(e) => {
+                  //@ts-ignore
+                  if (!/\d+/.test(Number(e.target.value))) return;
+                  const som = Number(e.target.value) * rate[CurrencyType.RUB];
+                  const converted = som
+                    ? Number(som) / rate[selectedBuyCurrency]
+                    : 0;
+                  setInputData((v) => ({
+                    ...v,
+                    sell: Number(e.target.value),
+                    buy: converted - (converted / 100) * 5,
+                  }));
+                }}
+              />
             </Box>
             <Box
               component="form"
@@ -191,21 +190,19 @@ const TelegramPage = () => {
               noValidate
               autoComplete="off"
             >
-              <div>
-                <TextField
-                  id="standard-select-currency"
-                  label="валюта"
-                  select
-                  variant="outlined"
-                  value={CurrencyType.RUB}
-                >
-                  <MenuItem value={CurrencyType.RUB}>
-                    <img src={russia} />
-                    <span>⠀</span>
-                    {CurrencyType.RUB}
-                  </MenuItem>
-                </TextField>
-              </div>
+              <TextField
+                id="standard-select-currency"
+                label="валюта"
+                select
+                variant="outlined"
+                value={CurrencyType.RUB}
+              >
+                <MenuItem value={CurrencyType.RUB}>
+                  <img src={russia} />
+                  <span>⠀</span>
+                  {CurrencyType.RUB}
+                </MenuItem>
+              </TextField>
             </Box>
           </div>
           <div className="formTop">
@@ -246,34 +243,32 @@ const TelegramPage = () => {
               noValidate
               autoComplete="off"
             >
-              <div>
-                <TextField
-                  // style={{ width: "100%" }}
-                  id="outlined-basic"
-                  label="Вы получаете"
-                  defaultValue="Вы получаете"
-                  variant="outlined"
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  value={inputData.buy.toFixed(2)}
-                  // onChange={(e) => {
-                  //   //@ts-ignore
-                  //   if (!/\d+/.test(Number(e.target.value))) return;
+              <TextField
+                // style={{ width: "100%" }}
+                id="outlined-basic"
+                label="Вы получаете"
+                defaultValue="Вы получаете"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={inputData.buy.toFixed(2)}
+                // onChange={(e) => {
+                //   //@ts-ignore
+                //   if (!/\d+/.test(Number(e.target.value))) return;
 
-                  //   const som = Number(e.target.value) * rate[selectedBuyCurrency];
-                  //   const converted = som
-                  //     ? Number(som) / rate[CurrencyType.RUB]
-                  //     : 0;
+                //   const som = Number(e.target.value) * rate[selectedBuyCurrency];
+                //   const converted = som
+                //     ? Number(som) / rate[CurrencyType.RUB]
+                //     : 0;
 
-                  //   setInputData((v) => ({
-                  //     ...v,
-                  //     buy: +e.target.value,
-                  //     sell: converted + (converted / 100) * 5,
-                  //   }));
-                  // }}
-                />
-              </div>
+                //   setInputData((v) => ({
+                //     ...v,
+                //     buy: +e.target.value,
+                //     sell: converted + (converted / 100) * 5,
+                //   }));
+                // }}
+              />
             </Box>
             <Box
               component="form"
@@ -283,41 +278,39 @@ const TelegramPage = () => {
               noValidate
               autoComplete="off"
             >
-              <div>
-                <TextField
-                  id="outlined-select-currency-native"
-                  select
-                  label="валюта"
-                  defaultValue="РУБ"
-                  variant="outlined"
-                  value={selectedBuyCurrency}
-                  onChange={(e) => {
-                    setSelectedBuyCarrency(e.target.value as CurrencyType);
-                    setInputData((v) => ({
-                      ...v,
-                      buy: rate[e.target.value as CurrencyType],
-                      sell: rate[CurrencyType.RUB],
-                    }));
-                  }}
-                >
-                  <MenuItem value={CurrencyType.USD}>
-                    <img src={usa} />
-                    <span>⠀</span>
-                    {CurrencyType.USD}
-                  </MenuItem>
-                  <MenuItem value={CurrencyType.EUR}>
-                    <img src={euro} />
-                    <span>⠀</span>
-                    {CurrencyType.EUR}
-                  </MenuItem>
-                </TextField>
-              </div>
+              <TextField
+                id="outlined-select-currency-native"
+                select
+                label="валюта"
+                defaultValue="РУБ"
+                variant="outlined"
+                value={selectedBuyCurrency}
+                onChange={(e) => {
+                  setSelectedBuyCarrency(e.target.value as CurrencyType);
+                  setInputData((v) => ({
+                    ...v,
+                    buy: rate[e.target.value as CurrencyType],
+                    sell: rate[CurrencyType.RUB],
+                  }));
+                }}
+              >
+                <MenuItem value={CurrencyType.USD}>
+                  <img src={usa} />
+                  <span>⠀</span>
+                  {CurrencyType.USD}
+                </MenuItem>
+                <MenuItem value={CurrencyType.EUR}>
+                  <img src={euro} />
+                  <span>⠀</span>
+                  {CurrencyType.EUR}
+                </MenuItem>
+              </TextField>
             </Box>
           </div>
 
           <div className="divtelega">
             <TextField
-              // style={{ width: "90%" }}
+              style={{ width: "100%" }}
               className="telegrammm"
               id="outlined-textarea"
               label="Ваш Telegram"
@@ -351,14 +344,6 @@ const TelegramPage = () => {
             className="centerTextt"
             style={{ padding: "0rem 1rem 0rem 0rem" }}
           >
-            {/* <div className="komission">
-      Комиссия за перевод:
-      <span
-        style={{ fontWeight: "700", color: "black", marginLeft: "5px" }}
-      >
-        5.00%
-      </span>
-    </div> */}
             <div className="minimalSumm">
               *Минимальная сумма перевода:
               <span
